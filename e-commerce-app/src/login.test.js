c0319aacbb3f52a51b87561961c92b66e192c30b
Soumyadeep Das <soumyadeep.das@techmojo.in>
2022-08-01 12:55:15 +0530

    initial commit

---
+++ e-commerce-app/src/login.test.js
@@ -0,0 +1,22 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import Login from './Login'
+
+const mockEvent = jest.fn();
+jest.mock ("react-router-dom",()=>({
+    ...jest.requireActual("react-router-dom"),
+    useNavigate: ()=>mockEvent,
+}));
+
+test("mock navigate to next page", ()=>{
+    render(<Login/>);
+
+    var email = screen.getByPlaceholderText("Enter email");
+    fireEvent.change(email, {target:{ value: 'soumya@gmail.com' }});
+
+
+    var password = screen.getByPlaceholderText("Password")
+    fireEvent.change(password,{target:{value: "Rkm@12353" }});
+
+    var button = screen.getByRole('button')
+    fireEvent.click(button)
+})
